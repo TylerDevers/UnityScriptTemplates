6e762de3ace8ada60d0be9cd9b4e47e0e0238857
Tyler Devers <TylerDevers@users.noreply.github.com>
2023-01-15 19:58:39 -0800

    refactored code into functions.
    linked animator. TODO Code animations

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private new Rigidbody2D rigidbody;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    Animator animator;
     [SerializeField] private float speed = 8f;
     [SerializeField] private float jumpingPower = 21f;
     float acceleration;
@@ -22,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake() {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         acceleration = speed * 2;
     }
 
@@ -30,19 +32,11 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
-        // full jump press yields full jump
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpingPower);
-        }
-
-        // early jump release yields partial jump
-        if (Input.GetButtonUp("Jump") && rigidbody.velocity.y > 0f)
-        {
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y * 0.5f);
-        }
-
+        Jump();
+        AnimatePlayer();
         FlipSprite();
+        IsGrounded();
+        print(rigidbody.velocity);
     }
 
 
@@ -64,6 +58,22 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
+    void Jump() {
+        // full jump press yields full jump
+        if (Input.GetButtonDown("Jump") && !jumping)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpingPower);
+            jumping = true;
+        }
+
+        // early jump release yields partial jump
+        if (Input.GetButtonUp("Jump") && rigidbody.velocity.y > 0f)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y * 0.5f);
+        }
+    }
+
+
     void FlipSprite() {
         Vector3 tempScale = transform.localScale;
 
@@ -82,6 +92,20 @@ public class PlayerMovement : MonoBehaviour
 
     void AnimatePlayer() {
 
+        if (!jumping) {
+            if (horizontalInput > 0f && rigidbody.velocity.x < 0) {
+                animator.SetBool("Slide", true);
+            }
+            else if (horizontalInput > 0f || horizontalInput < 0f) {
+                animator.SetBool("Run", true);
+            } else if (horizontalInput == 0) {
+                animator.SetBool("Run", false);
+            }
+
+        }
+        
+
+        
         if (horizontalInput > 0f && rigidbody.velocity.x < 0f ) {
             // skid direction
         }
